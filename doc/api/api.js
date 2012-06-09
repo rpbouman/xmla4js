df1b6eec465b41e067111acb656e602deac0b4cd
roland.bouman <roland.bouman@71d1713c-f0ca-11de-9456-cf00f3bddd4e>
2012-06-09 20:58:45 +0000

    Re-adding api docs to svn. Let's see if we fare better this time.

---
+++ doc/api/api.js
@@ -0,0 +1,22 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Xmla",
+        "Xmla.Dataset",
+        "Xmla.Dataset.Axis",
+        "Xmla.Dataset.Cellset",
+        "Xmla.Exception",
+        "Xmla.Rowset"
+    ],
+    "modules": [
+        "xmla"
+    ],
+    "allModules": [
+        {
+            "displayName": "xmla",
+            "name": "xmla",
+            "description": "This is xmla4js - a stand-alone, cross-browser javascript library for working with \"XML for Analysis\".\n  XML for Analysis (XML/A) is a vendor-neutral industry-standard protocol for OLAP services over HTTP.\n  xmla4js enables web-browser-based analytical business intelligence applications."
+        }
+    ]
+} };
+});
