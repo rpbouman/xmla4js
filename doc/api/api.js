c12c0620b8f27c9fd412cfbe9c8f529012f945f8
rpbouman <Roland.Bouman@gmail.com>
2014-02-17 13:23:57 +0100

    Updated api docs.

--- doc/api/api.js
+++ doc/api/api.js
@@ -15,7 +15,7 @@ YUI.add("yuidoc-meta", function(Y) {
         {
             "displayName": "xmla",
             "name": "xmla",
-            "description": "This is xmla4js - a stand-alone, cross-browser javascript library for working with \"XML for Analysis\".\n  XML for Analysis (XML/A) is a vendor-neutral industry-standard protocol for OLAP services over HTTP.\n  xmla4js enables web-browser-based analytical business intelligence applications."
+            "description": "This is xmla4js - a stand-alone javascript library for working with \"XML for Analysis\".\n  XML for Analysis (XML/A) is a vendor-neutral industry-standard protocol for OLAP services over HTTP.\n  Xmla4js is cross-browser and node.js compatible and enables web-browser-based analytical business intelligence applications.\n  Xmla4js can be loaded as a common js or amd module."
         }
     ]
 } };
