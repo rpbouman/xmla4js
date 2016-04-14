0b641417b51f9fd561ebecc63c76eb3123cf28b3
rpbouman <roland.bouman@gmail.com>
2016-04-14 23:51:54 +0200

    New version

--- build/minify.bat
+++ build/minify.bat
@@ -5,9 +5,9 @@ REM see: http://developer.yahoo.com/yui/compressor/
 REM 
 
 REM Set the XMLA_HOME variable to the directory to which you checked out the xmla4js project directory 
-SET XMLA_HOME="D:\Projects\xmla4js"
+SET XMLA_HOME="C:\roland\projects\xmla4js"
 
 REM set the YUI_COMPRESSOR variable to the location of the YUI compressor jar file.
-SET YUI_COMPRESSOR="D:\Applications\yui\yuicompressor-2.4.2\build\yuicompressor-2.4.2.jar"
+SET YUI_COMPRESSOR="C:\jars\yuicompressor-2.4.8.jar"
 
 java -jar "%YUI_COMPRESSOR%" "%XMLA_HOME%/src/Xmla.js" -o "%XMLA_HOME%/js/Xmla-min.js" --charset utf-8 
--- doc/api/api.js
+++ doc/api/api.js
@@ -17,6 +17,7 @@ YUI.add("yuidoc-meta", function(Y) {
             "name": "xmla",
             "description": "This is xmla4js - a stand-alone javascript library for working with \"XML for Analysis\".\n  XML for Analysis (XML/A) is a vendor-neutral industry-standard protocol for OLAP services over HTTP.\n  Xmla4js is cross-browser and node.js compatible and enables web-browser-based analytical business intelligence applications.\n  Xmla4js can be loaded as a common js or amd module."
         }
-    ]
+    ],
+    "elements": []
 } };
 });
